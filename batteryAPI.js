967c50174624c14fa8da86a1203708ee5e48e75a
Siesqo <jeroen@siesqo.be>
2013-08-19 09:46:54 +0200

    Initial commit

---
+++ batteryAPI.js
@@ -0,0 +1,69 @@
+/**
+ * jsFrontend Battery API
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+jsFrontendBatteryAPI =
+{
+	battery: false,
+	init: function()
+	{
+		// get the battery
+		battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery;
+
+		// redefine core battery
+		if(battery != 'undefined') jsFrontendBatteryAPI.battery = battery;
+	},
+
+	/**
+	 * Is charging
+	 *
+	 * @return bool
+	 */
+	isCharging: function()
+	{
+		return (jsFrontendBatteryAPI.hasBattery()) ? jsFrontendBatteryAPI.battery.charging : false;
+	},
+
+	/**
+	 * Is supported
+	 *
+	 * @return bool
+	 */
+	isSupported: function()
+	{
+		return !!(jsFrontendBatteryAPI.battery);
+	},
+
+	/**
+	 * Get charging time
+	 *
+	 * @return float
+	 */
+	getChargingTime: function()
+	{
+		return (jsFrontendBatteryAPI.hasBattery()) ? jsFrontendBatteryAPI.battery.chargingTime : 0;	
+	},
+
+	/**
+	 * Get discharging time
+	 *
+	 * @return float
+	 */
+	getDischargingTime: function()
+	{
+		return (jsFrontendBatteryAPI.hasBattery()) ? jsFrontendBatteryAPI.battery.dischargingTime : 0;	
+	},
+
+	/**
+	 * Get level
+	 *
+	 * @return float
+	 */
+	getLevel: function()
+	{
+		return (jsFrontendBatteryAPI.hasBattery()) ? jsFrontendBatteryAPI.battery.level : 0;	
+	}
+}
+
+$(jsFrontendBatteryAPI.init);
---
+++ batteryAPITest.js
@@ -0,0 +1,48 @@
+/**
+ * jsFrontend Battery API test
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+jsFrontendBatteryAPITest =
+{
+	init: function()
+	{
+		if(jsFrontendBatteryAPI.isSupported())
+		{
+			// test some properties
+			console.warn("Battery charging: ", jsFrontendBatteryAPI.isCharging()); // true
+			console.warn("Battery level: ", jsFrontendBatteryAPI.getLevel()); // 0.58
+			console.warn("Battery discharging time: ", jsFrontendBatteryAPI.getDischargingTime());
+	
+			// bind event for charging change
+			battery.addEventListener("chargingchange", function(e)
+			{
+				console.warn("Battery charge change: ", jsFrontendBatteryAPI.isCharging());
+			}, false);
+	
+			// bind event for charging time change
+			battery.addEventListener("chargingtimechange", function(e)
+			{
+				console.warn("Battery charge time change: ", jsFrontendBatteryAPI.getChargingTime());
+			}, false);
+	
+			// bind event for discharging time change
+			battery.addEventListener("dischargingtimechange", function(e)
+			{
+				console.warn("Battery discharging time change: ", jsFrontendBatteryAPI.getDischargingTime());
+			}, false);
+	
+			// bind event for battery level change
+			battery.addEventListener("levelchange", function(e)
+			{
+				console.warn("Battery level change: ", jsFrontendBatteryAPI.getLevel());
+			}, false);
+		}
+		else
+		{
+			console.warn('Can\'t reach Battery API');
+		}
+	}
+}
+
+$(jsFrontendBatteryAPITest.init);
