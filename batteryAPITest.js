1af22ae2ec395a6dc49f21b78284279057eb5e5e
Siesqo <jeroen@siesqo.be>
2014-01-08 13:50:07 +0100

    Renamed class in test

--- batteryAPITest.js
+++ batteryAPITest.js
@@ -7,35 +7,35 @@ batteryAPITest =
 {
 	init: function()
 	{
-		if(jsFrontendBatteryAPI.isSupported())
+		if(batteryAPI.isSupported())
 		{
 			// test some properties
-			console.warn("Battery charging: ", jsFrontendBatteryAPI.isCharging()); // true
-			console.warn("Battery level: ", jsFrontendBatteryAPI.getLevel()); // 0.58
-			console.warn("Battery discharging time: ", jsFrontendBatteryAPI.getDischargingTime());
+			console.warn("Battery charging: ", batteryAPI.isCharging()); // true
+			console.warn("Battery level: ", batteryAPI.getLevel()); // 0.58
+			console.warn("Battery discharging time: ", batteryAPI.getDischargingTime());
 	
 			// bind event for charging change
 			battery.addEventListener("chargingchange", function(e)
 			{
-				console.warn("Battery charge change: ", jsFrontendBatteryAPI.isCharging());
+				console.warn("Battery charge change: ", batteryAPI.isCharging());
 			}, false);
 	
 			// bind event for charging time change
 			battery.addEventListener("chargingtimechange", function(e)
 			{
-				console.warn("Battery charge time change: ", jsFrontendBatteryAPI.getChargingTime());
+				console.warn("Battery charge time change: ", batteryAPI.getChargingTime());
 			}, false);
 	
 			// bind event for discharging time change
 			battery.addEventListener("dischargingtimechange", function(e)
 			{
-				console.warn("Battery discharging time change: ", jsFrontendBatteryAPI.getDischargingTime());
+				console.warn("Battery discharging time change: ", batteryAPI.getDischargingTime());
 			}, false);
 	
 			// bind event for battery level change
 			battery.addEventListener("levelchange", function(e)
 			{
-				console.warn("Battery level change: ", jsFrontendBatteryAPI.getLevel());
+				console.warn("Battery level change: ", batteryAPI.getLevel());
 			}, false);
 		}
 		else
